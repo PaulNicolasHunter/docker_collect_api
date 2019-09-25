b5cc4381698eccded8f13aaa31a925950744f6c6
PaulNicolasHunter <choudhary.vivek98@gmail.com>
2019-09-25 17:05:54 +0530

    adding testing and dummy files

--- tester.py
+++ tester.py
@@ -9,6 +9,18 @@ data = {
 for _ in dat:
    data.update({_: dat[_].values.tolist()}) 
 
+"""
+    upload route end-point. Expected data structure for post, have a look at dummy_data.xlsx
+    
+    data ->
+    {
+        col : ['name', 'age', 'DOB'],
+        name: ['jay', 'ron', 'katy'...],
+        age: [25, 42, 21 ...],
+        DOB: ['2/5/1994', '5/3/1977', '6/8/1998'...],
+    }
+
+"""
 a = requests.post( 'http://localhost:3000/upload',  data=data)
 
 print(a.text)
